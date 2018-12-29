ad1a4c3875bd577083ce5c2240711efcf0383801
Mushu <mouhou.elyas@gmail.com>
2018-12-29 14:06:29 +0100

    nettoyage

--- CorrectionTP.R
+++ CorrectionTP.R
@@ -1,4 +1,23 @@
-
+library(skimr)
+# Importation des données
 dt = read.csv("Data/Inhibiteurs_GPR54.dat (copie).txt", sep = "\t")
 
-dt.hclust = hclust(dt)
+# Description des données
+summary(dt)
+skimr::skim(dt)
+sum(is.na(dt))
+
+#Nettoyage
+dt[which(dt=="", arr.ind = T)]= NA
+dt1=dt
+dt1$N<-NULL
+dt1$X<-NULL
+dt1$StructureChemDraw<-NULL
+dt1 = dt1[-which(is.na(dt1$C.ter)),]
+
+#Check NA des seq
+sum(is.na(dt1[,5:16]))
+
+mydist = dist(dt1[,5:16], method = "manhattan")
+# MAS QUE FAAAAAAAAAAAAAAAIIIIIIRE
+dt.hclust = hclust(dt1[,5:16])
